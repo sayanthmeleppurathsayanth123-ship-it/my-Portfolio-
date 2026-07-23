8ad67fd35dd0496a3092a46af612eff5f91e73a6
Sayanth MP <sayanthmeleppurathsayanth123@gmail.com>
2026-07-23 12:30:40 +0530

    Refactor skills animation code for clarity

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,18 +105,22 @@
   window.addEventListener('load', aosInit);
 
   /**
-   * Animate the skills items on reveal
-   */
-  let skillsAnimation = document.querySelectorAll('.skills-animation');
-  skillsAnimation.forEach((item) => {
-    new Waypoint({
-      element: item,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = item.querySelectorAll('.progress .progress-bar');
-        progress.forEach(el => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%';
-        });
-      }
-    });
+   /**
+ * Animate the skills items on reveal
+ */
+let skillsAnimation = document.querySelectorAll('.skills-animation');
+
+skillsAnimation.forEach((item) => {
+  new Waypoint({
+    element: item,
+    offset: '80%',
+    handler: function () {
+      let progress = item.querySelectorAll('.progress .progress-bar');
+      progress.forEach((el) => {
+        el.style.width = el.getAttribute('aria-valuenow') + '%';
+      });
+    }
   });
+});
+
+})();
